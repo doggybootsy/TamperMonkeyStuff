b974aca0601fd26351bc924141e52e4face0fae9
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2021-06-28 17:03:24 -0700

    Create EnablePIP.js

---
+++ DisneyPlus/EnablePIP/EnablePIP.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         Enable PIP
+// @namespace    https://github.com/doggybootsy/TamperMonkeyStuff
+// @version      0.1
+// @description  Allows the ability to have PIP again
+// @author       Doggybootsy#0001
+// @match        *://www.disneyplus.com/video/*
+// @icon         https://www.google.com/s2/favicons?domain=disneyplus.com
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const targetNode = document.getElementById('app_index');
+    const config = { attributes: true, childList: true, subtree: true };
+    const callback = function(mutationsList, observer) {
+        for(const mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                if(document.querySelector('[disablepictureinpicture]')){
+                    document.querySelector('[disablepictureinpicture]').removeAttribute('disablepictureinpicture')
+                }
+            }
+        }
+    };
+    const observer = new MutationObserver(callback);
+    observer.observe(targetNode, config);
+})();
